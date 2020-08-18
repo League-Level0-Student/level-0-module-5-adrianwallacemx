5c657ce276d1f121ddd4a908ad7a00975facd481
root <root@3efd52e05563>
2020-08-18 00:57:56 +0000

    for loop gauntlet

---
+++ src/_06_for_loop_gauntlet/ForLoopGauntlet.java
@@ -0,0 +1,30 @@
+package _06_for_loop_gauntlet;
+
+public class ForLoopGauntlet {
+public static void main(String[] args) {
+//	for (int i = 0; i < 101; i++) {
+//		System.out.println(i);
+//	}
+//	for (int i = 100; i >-1 ; i--) {
+//		System.out.println(i);
+//	}
+	
+//	for (int i =2; i < 100; i+=2) {
+//		System.out.println(i);
+//	}
+//	for (int i = 1; i < 100; i+=2) {
+//		System.out.println(i);
+//	}
+//	for (int i = 1; i < 501; i++) {
+//		System.out.println(i);
+//	}
+//	for (int i = 7; i <778; i+=7) {
+	//	System.out.println(i);
+//	}
+	for (int i = 0; i <15; i++) {
+		System.out.println("in " + (2005 + i) + "  I was " + i + " years old");
+	}
+	
+	}
+}
+
